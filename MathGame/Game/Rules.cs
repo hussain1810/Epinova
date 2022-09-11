8e762c86d35bf16efe02d9389a24945645798001
Hussain <hussain_619_@hotmail.com>
2022-09-11 15:40:46 +0200

    Task one FizzBuzz game completed

---
+++ MathGame/Game/Rules.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MathGame.Game 
+{
+    class Rules 
+    {
+        public string name;
+        public int number;
+
+        public Rules(
+            string name,
+            int number
+        ){
+            this.name = name;
+            this.number = number;
+        }
+    }
+}
---
+++ MathGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MathGame
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            List<Game.Rules> rules = new List<Game.Rules> 
+            {
+                new Game.Rules("Fizz", 3),
+                new Game.Rules("Buzz", 5)
+            };
+            Game.Game fizzBuzz = new Game.Game(1, 100, rules);
+            fizzBuzz.RunGame();
+        }
+    }
+}
